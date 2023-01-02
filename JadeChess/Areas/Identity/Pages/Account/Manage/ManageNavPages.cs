49cb8326a151635ebed2b5a4f47026bfe3582df1
tsziming <98565987+tsziming@users.noreply.github.com>
2023-01-02 11:48:13 +0100

    Update Identity Theme

--- JadeChess/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ JadeChess/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -117,7 +117,7 @@ namespace  JadeChess.Areas.Identity.Pages.Account.Manage
         {
             var activePage = viewContext.ViewData["ActivePage"] as string
                 ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
-            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "bg-danger text-dark" : null;
         }
     }
 }
